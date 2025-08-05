02e0056145e7536b78f3ba36697e8e27159c85ae
saadferozee <saadferozee@gmail.com>
2025-08-05 15:30:08 +0600

    added practice of break in loop

--- tasks/break/2 sum upto 100.js
+++ tasks/break/2 sum upto 100.js
@@ -1,3 +1,16 @@
 /* 
 Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
+
+
+let num = 1
+let sum = null;
+while (num <= 1000) {
+    // console.log(num);
+    sum = sum + num;
+    console.log(sum);
+    if (sum >= 100) {
+        break;
+    }
+    num++;
+}
--- tasks/break/3 square break.js
+++ tasks/break/3 square break.js
@@ -1,3 +1,10 @@
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+
+for (let num = 2; num <= 100; num ++) {
+    console.log(num)    
+    if ( Math.sqrt(num) % 1 === 0) {
+        break
+    }
+}

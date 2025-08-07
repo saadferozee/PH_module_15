741b63d079bc61a6ad126b9bac6ba19ce33526c2
saadferozee <saadferozee@gmail.com>
2025-08-07 23:46:45 +0600

    added all tasks

--- tasks/continue/2 skip-five.js
+++ tasks/continue/2 skip-five.js
@@ -6,6 +6,14 @@ for (let i = 55; i <= 85; i ++) {
     if (i % 5 === 0) {
         continue;
     } else {
-        console.log(i);
+        // console.log(i);
     }
 }
+
+
+let array = [1, 2, 3, 4, 5, 6];
+
+console.log(array);
+let reverse = array.reverse();
+console.log(reverse);
+console.log(array);
